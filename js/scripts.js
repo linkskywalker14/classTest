37f76f4ba91a46819036d7436469b26e81e1201c
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2020-10-15 17:05:18 -0700

    Cleaned up some excessive if/then statements with loops.

--- js/scripts.js
+++ js/scripts.js
@@ -2,56 +2,52 @@
 //This up here is what they done call "business logic."
 
 function findClass(playstyle) {
-  let fighter = 0;
-  let ranger = 0;
-  let thief = 0;
-  let cleric = 0;
-  let magician = 0;
+  let fighter = [0, "Fighter"];
+  let ranger = [0, "Ranger"];
+  let thief = [0, "Thief"];
+  let cleric = [0, "Cleric"];
+  let magician =[0, "Magic user"];
   
+  //Read responses, each answer adds 1 point to a corresponding result.
+
   playstyle.forEach(function(choice){
     if (choice === "Fighter"){
-      fighter += 1;
+      fighter[0] += 1;
     };
     if (choice === "Ranger"){
-      ranger += 1;
+      ranger[0] += 1;
     };
     if (choice === "Thief"){
-      thief += 1;
+      thief[0] += 1;
     };
     if (choice === "Cleric"){
-      cleric += 1;
+      cleric[0] += 1;
     };
     if (choice === "Magician"){
-      magician += 1;
+      magician[0] += 1;
     };
   });
 
-  const biggest = Math.max(fighter, ranger, thief, cleric, magician);
+  //Check which results have the most points, and push them to a new array
+
+  const options = [fighter, ranger, thief, cleric, magician]
+  const biggest = Math.max(fighter[0], ranger[0], thief[0], cleric[0], magician[0]);
   let finalChoice = [];
 
-  
-  if (biggest === fighter){
-    finalChoice.push("Fighter");
-  };
-  if (biggest === ranger){
-    finalChoice.push("Ranger");
-  };
-  if (biggest === thief){
-    finalChoice.push("Thief");
-  };
-  if (biggest === cleric){
-    finalChoice.push("Cleric");
-  };
-  if (biggest === magician){
-    finalChoice.push("Magic User");
-  };
+  options.forEach(function(element){
+    if (element[0] === biggest){
+      finalChoice.push(element[1]);
+    }
+  })
+
+  //Check to see if there there are any tied results. 
 
   if (finalChoice[2]){
     return "Bard. That's...nice."
-  } else if (finalChoice[1] + ", won't that be fun?"){
+  } else if (finalChoice[1]){
     return "Muticlass " + finalChoice[1] + " / " + finalChoice[0] + ", won't that be fun?";
   } else {
-    return finalChoice[0];
+    return finalChoice[0] + ", won't that be fun?";
   }
 
 };
